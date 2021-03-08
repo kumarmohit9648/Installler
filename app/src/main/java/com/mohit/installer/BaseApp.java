46acd763eaa4bcd21ec88de2da63e9d1a7ca7fb9
mohit <mohit.kumar@pristyncare.com>
2021-03-08 20:44:22 +0530

    add fileTree

--- app/src/main/java/com/mohit/installer/BaseApp.java
+++ app/src/main/java/com/mohit/installer/BaseApp.java
@@ -2,13 +2,13 @@ package com.mohit.installer;
 
 import android.app.Application;
 
-import com.downloader.PRDownloader;
+//import com.downloader.PRDownloader;
 
 public class BaseApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        PRDownloader.initialize(getApplicationContext());
+        // PRDownloader.initialize(getApplicationContext());
     }
 }
